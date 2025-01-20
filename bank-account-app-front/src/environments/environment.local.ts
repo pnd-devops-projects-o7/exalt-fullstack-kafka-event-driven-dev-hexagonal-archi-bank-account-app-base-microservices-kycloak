07cb0a8ddf277387871679ed6a2cb4bc05c9201d
placidenduwayo <placide.nd@gmail.com>
2025-01-20 11:56:34 +0100

    feat(InputServiceImplTest): perform unit and integration tests

---
+++ bank-account-app-front/src/environments/environment.local.ts
@@ -0,0 +1,26 @@
+import { KeycloakOptions } from "keycloak-angular";
+
+// The list of file replacements can be found in `angular.json`.
+const keycloakInitOptions: KeycloakOptions = {
+    config: {
+        url: 'http://localhost:8380',
+        realm: 'pnd_realm',
+        clientId: 'exalt-hexagonal-archi-kafka-keycloak-frontend-app'
+    },
+    enableBearerInterceptor: true,
+    bearerPrefix: 'Bearer',
+    bearerExcludedUrls: ['/assets'],
+    initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+            window.location.origin + '/assets/silent-check-sso.html'
+    }
+}
+
+export const environment = {
+    production: false,
+    gatewayApiService: "http://localhost:8279/api",
+    logoutUrl: "http://localhost:4200",
+    keycloackConfInit: keycloakInitOptions
+
+};
